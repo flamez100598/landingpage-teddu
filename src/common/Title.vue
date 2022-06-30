893816d033690a6610eae4c8c82b1a40cd34d196
DungPQ98 <philips@creatorchain.network>
2022-06-30 07:29:59 +0700

    update code

---
+++ src/common/Title.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="title-container">
+    <div class="relative">{{title}}
+        <div class="absolute">
+            <img src="~/static/icons/hat.png" alt="">
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/HowItWork/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card-container">
+    <div class="inner-card">
+        <div class="icon-card">
+            <img :src="srcIcon" alt="">
+        </div>
+        <div class="title-card">
+            <img :src="src" alt="">
+        </div>
+        <div>{{content}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ props: {
+    src: String,
+    srcIcon: String,
+    content: String
+ }
+}
+</script>
+
+<style>
+
+</style>
